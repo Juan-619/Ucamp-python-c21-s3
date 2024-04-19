b57ce3d9a804d07a73b55ef7bbb805ad7d90d3aa
Juan pablo Garcia <Juan-619@live.com>
2024-04-18 23:07:50 -0600

    agregar otro prin

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hola mundo")
+numero = input("Por favor ingrese un numero: ")
+numero = int(numero)
+print (numero)
+numero = numero/3
+print(numero)
+
+#if numero > 2:
+#    print("True")
+#else:
+#    print("False")
+
+print (numero > 2)
